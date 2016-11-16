08549061805da5618cddd6333813a68d6848860e
SocialChaz <chaz@socialdriver.com>
2016-11-16 06:06:09 -0500

    script updates

--- index.js
+++ index.js
@@ -9,30 +9,42 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  var user = [];
-  socket.on('test', function(msg) {
-    getUsers(msg);
-    socket.emit('users', user)
-    
-    function getUsers(u) {
-      user.push(u)
-    }
-    console.log('a user connected');
-    
-});
-  
-  
-  socket.on('disconnect', function() {
-    console.log('a user disconnected');
-  });
-    
-  
-  
-});
-
-
-
 http.listen(9000, function(){
   console.log('listening on *:9000');
 });
+
+users = [];
+connections =[];
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+	
+	// Disconnect
+	socket.on('disconnect', function(data) {
+		//if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);	
+	});
+	
+	//Send Message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+	
+	// New User
+	socket.on('new-user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+		console.log(users);
+	});
+	
+	function updateUsernames(){
+		io.sockets.emit('get-users', users);
+	}
+});
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,24 @@
 var app = angular.module("chatApp", []);
 
 app.controller("chatCtrl", function($scope, socket) {
-  var user = [];
-  $scope.users =[];
   $scope.hideMe = false;
-  $scope.getName = function(c){
+  $scope.getName = function(users){
     var day = new Date;
     var id = day.getTime();
-    user.push(c,id);
+
     $scope.hideMe = true;
     $('#name').val(' ');
+	
     // Pushes data to server
-    socket.emit('test', user);
-    // Get Users from server
-    socket.on('users', function(user) {
-      $scope.users.push($.extend({}, user[0]));
-      $scope.test = $scope.users[0];
-      $('#user-list').append('<li> '+$scope.users[0][0]+'</li>');
-    });
-
+    socket.emit('new-user', users.toString());
+    
   }
+  
+  socket.on('get-users', function(user) {
+  
+	for(i = 0; i < user.length; i++){
+		$('#user-list').append('<li>'+user[i]+'</li>');
+	}
+  });
   
 });
