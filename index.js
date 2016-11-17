0b262091f4f3702a69770b3e7605fd47ea25d2ad
202chaz <chaz.haskins@gmail.com>
2016-11-17 09:20:49 -0500

    fixed js files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var express = require('express');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
 
 app.use(express.static('public'))
 
@@ -18,29 +18,28 @@ connections =[];
 
 io.sockets.on('connection', function(socket){
 	connections.push(socket);
-	console.log('Connected: %s sockets connected', connections.length);
-	
-	// Disconnect
-	socket.on('disconnect', function(data) {
-		//if(!socket.username) return;
-		users.splice(users.indexOf(socket.username), 1);
-		updateUsernames();
-		connections.splice(connections.indexOf(socket), 1);
-		console.log('Disconnected: %s sockets connected', connections.length);	
-	});
-	
-	//Send Message
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, user: socket.username});
-	});
-	
+	//console.log('Connected: %s sockets connected', connections.length);
+  
 	// New User
 	socket.on('new-user', function(data, callback){
 		callback(true);
 		socket.username = data;
 		users.push(socket.username);
 		updateUsernames();
-		console.log(users);
+	});
+  
+	// Disconnect
+	socket.on('disconnect', function(data) {
+		if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		//console.log('Disconnected: %s sockets connected', connections.length);	
+	});
+	
+	//Send Message
+	socket.on('send-message', function(data){
+		io.sockets.emit('new-message', {msg: data, user: socket.username});
 	});
 	
 	function updateUsernames(){
--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,36 @@ var app = angular.module("chatApp", []);
 
 app.controller("chatCtrl", function($scope, socket) {
   $scope.hideMe = false;
+  
   $scope.getName = function(users){
     var day = new Date;
     var id = day.getTime();
-
-    $scope.hideMe = true;
-    $('#name').val(' ');
-	
     // Pushes data to server
-    socket.emit('new-user', users.toString());
-    
+    socket.emit('new-user', users.toString(), function(data) {
+      if(data) {
+        //console.log(data);
+      }
+    }); 
+    $('#name').val(' ');
+    $scope.hideMe = true;   
   }
   
   socket.on('get-users', function(user) {
+    var html = '';
+  	for(i = 0; i < user.length; i++){
+  		html += '<li>'+user[i]+'</li>';
+  	}
+    $('#user-list').html(html);
+  });
   
-	for(i = 0; i < user.length; i++){
-		$('#user-list').append('<li>'+user[i]+'</li>');
-	}
+  
+  $scope.getMessage = function(data) {
+    console.log(data);
+    socket.emit('send-message', data);
+    $('#message').val('');
+  }
+  socket.on('new-message', function(data) {
+    $('#chat').append('<div class="chatMessage">'+data.msg+'</div>');
   });
   
 });
